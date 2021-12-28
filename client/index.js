c10c742d4bba20303c63b669b9b4cd4db0fcdb9b
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-12-28 21:36:30 +0100

    Scrolling chat messages to bottom when a new message arrives
    Also handles the case when a user scrolls in the chat history.
    In this case the scroll position stays the same.

--- client/index.js
+++ client/index.js
@@ -1,12 +1,31 @@
 const $chatMessages = document.querySelector('.chat__messages')
 const $chatInput = document.querySelector('.chat__input')
 
-const socket = new WebSocket('ws://localhost:8081')
+let isInitialLoad = true
+let isScrollingInHistory = false
+
+function determineBottomScrollPosition() {
+  return $chatMessages.scrollHeight - $chatMessages.clientHeight
+}
+
+function scrollDown() {
+  $chatMessages.scrollTop = determineBottomScrollPosition()
+}
+
+const socket = new WebSocket('ws://192.168.178.22:8081')
 
 socket.addEventListener('message', async function onMessage(event) {
   const $message = document.createElement('div')
   $message.textContent = await event.data
   $chatMessages.appendChild($message)
+  if (isInitialLoad || !isScrollingInHistory) {
+    scrollDown()
+  }
+  isInitialLoad = false
+})
+
+$chatMessages.addEventListener('scroll', function onScroll() {
+  isScrollingInHistory = $chatMessages.scrollTop < determineBottomScrollPosition()
 })
 
 function submitMessage() {
